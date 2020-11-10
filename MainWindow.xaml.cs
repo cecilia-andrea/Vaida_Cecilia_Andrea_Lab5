218cd7026adee997cbb23909559b7dd1e3079594
CECILIA-ANDREA VAIDA <cecilia.vaida@stud.ubbcluj.ro>
2020-11-10 10:32:42 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Vaida_Cecilia_Andrea_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
+
+
+namespace Vaida_Cecilia_Andrea_Lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+    public partial class MainWindow : Window
+    {
+        static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["PhoneNumbers"].ConnectionString;
+        ActionState action = ActionState.Nothing;
+        PhoneNumbersDataSet phoneNumberDataSet = new PhoneNumbersDataSet();
+        PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter tblPhoneNumbersAdapter = new PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter(connectionString);
+        Binding txtPhoneNumberBinding = new Binding();
+        Binding txtSubscriberBinding = new Binding();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            grdMain.DataContext = phoneNumberDataSet.PhoneNumbers;
+            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
+            txtSubscriberBinding.Path = new PropertyPath("Subscriber");
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+        }
+        private void lstPhoneLoad()
+        {
+            tblPhoneNumbersAdapter.Fill(phoneNumberDataSet.PhoneNumbers);
+        }
+
+        private void grdMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstPhoneLoad();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("Close Application?","Question",MessageBoxButton.YesNo,MessageBoxImage.Warning)==MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            PhoneNumbersDataSet phoneNumbersDataSet = ((PhoneNumbersDataSet)(this.FindResource(phoneNumbersDataSet)));
+            System.Windows.Data.CollectionViewSource phoneNumbersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("phoneNumberViewSource")));
+            phoneNumbersViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhone.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = "";
+            txtSubscriber.Text = "";
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhone.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhone.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+          
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Nothing;
+
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            //btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            lstPhone.IsEnabled = true;
+            btnPrevious.IsEnabled = true;
+            btnNext.IsEnabled = true;
+            txtPhoneNumber.IsEnabled = false;
+            txtSubscriber.IsEnabled = false;
+
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (action== ActionState.New)
+            {
+                try
+                {
+                    DataRow newRow = phoneNumberDataSet.PhoneNumbers.NewRow();
+                    newRow.BeginEdit();
+                    newRow["Phonenum"] = txtPhoneNumber.Text.Trim();
+                    newRow["Subscriber"] = txtSubscriber.Text.Trim();
+                    newRow.EndEdit();
+                    phoneNumberDataSet.PhoneNumbers.Rows.Add(newRow);
+                    tblPhoneNumbersAdapter.Update(phoneNumberDataSet.PhoneNumbers);
+                    phoneNumberDataSet.AcceptChanges();
+                }
+                catch(DataException ex)
+                {
+                    phoneNumberDataSet.RejectChanges();
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhone.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+            }
+            else
+                if (action==ActionState.Edit)
+                 {
+                    try
+                    {
+                        DataRow editRow = phoneNumberDataSet.PhoneNumbers.Rows[lstPhone.SelectedIndex];
+                        editRow.BeginEdit();
+                        editRow["Phonenum"] = txtPhoneNumber.Text.Trim();
+                        editRow["Subscriber"] = txtSubscriber.Text.Trim();
+                        editRow.EndEdit();
+                        tblPhoneNumbersAdapter.Update(phoneNumberDataSet.PhoneNumbers);
+                        phoneNumberDataSet.AcceptChanges();
+                    }
+                    catch( DataException ex)
+                    {
+                        phoneNumberDataSet.RejectChanges();
+                        MessageBox.Show(ex.Message);
+                    }
+
+                    btnNew.IsEnabled = true;
+                    btnEdit.IsEnabled = true;
+                    btnDelete.IsEnabled = true;
+                    btnSave.IsEnabled = false;
+                    btnCancel.IsEnabled = false;
+                    lstPhone.IsEnabled = true;
+                    btnPrevious.IsEnabled = true;
+                    btnNext.IsEnabled = true;
+                    txtPhoneNumber.IsEnabled = false;
+                    txtSubscriber.IsEnabled = false;
+
+                    txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                 txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+
+                }
+            else
+                if (action==ActionState.Delete)
+                {
+                    try
+                    {
+                    DataRow deleterow = phoneNumberDataSet.PhoneNumbers.Rows[lstPhone.SelectedIndex];
+                    deleterow.Delete();
+                    tblPhoneNumbersAdapter.Update(phoneNumberDataSet.PhoneNumbers);
+                    phoneNumberDataSet.AcceptChanges();
+                    }
+                    catch (DataException ex)
+                    {
+                    phoneNumberDataSet.RejectChanges();
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(ex.Message);
+                    }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhone.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+
+                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+            }
+            
+           
+
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView navigationView = CollectionViewSource.GetDefaultView(phoneNumberDataSet.PhoneNumbers);
+            navigationView.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView navigationView = CollectionViewSource.GetDefaultView(phoneNumberDataSet.PhoneNumbers);
+            navigationView.MoveCurrentToNext();
+        }
+    }
+}
